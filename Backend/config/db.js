eb6968291ed74315ba8e67bf7e87dc8c21c1067b
Kittinan <paykung123456789@gmail.com>
2025-10-05 02:48:32 +0700

    Create db.js

---
+++ Backend/config/db.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect
+    ("mongodb+srv://KittinanDev:<db_password>@soi7dev.vfh7wn1.mongodb.net/?retryWrites=true&w=majority&appName=Soi7Dev");
+    console.log("✅ MongoDB Connected");
+  } catch (error) {
+    console.error("❌ DB Connection Failed", error.message);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
